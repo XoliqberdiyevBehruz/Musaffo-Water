8273f58ab75e625d6fef174cd5e9d192f73078c4
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-04-22 15:57:29 +0500

    fix

--- common/filters.py
+++ common/filters.py
@@ -3,10 +3,10 @@ from common import models
 
 
 class ClientFilter(django_filters.FilterSet):
-    search = django_filters.CharFilter(field_name='full_name', lookup_expr='icontains')
-    search = django_filters.CharFilter(field_name='code_number', lookup_expr='icontains')
-    search = django_filters.CharFilter(field_name='numbers__number', lookup_expr='icontains')
+    full_name = django_filters.CharFilter(field_name='full_name', lookup_expr='icontains')
+    code_number = django_filters.CharFilter(field_name='code_number', lookup_expr='icontains')
+    phone_numbers = django_filters.CharFilter(field_name='numbers__number', lookup_expr='icontains')
 
     class Meta:
         model = models.Client
-        fields = ['region', 'orders__status', 'search']
+        fields = ['region', 'orders__status', 'full_name', 'code_number', 'phone_numbers']
