a948405225c0af6a67346fb3e8bad7a4146af64f
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-04-22 16:20:50 +0500

    fix

--- common/filters.py
+++ common/filters.py
@@ -1,12 +1,19 @@
 import django_filters 
+from django.db.models import Q
+
 from common import models 
 
 
 class ClientFilter(django_filters.FilterSet):
-    full_name = django_filters.CharFilter(field_name='full_name', lookup_expr='icontains')
-    code_number = django_filters.CharFilter(field_name='code_number', lookup_expr='icontains')
-    phone_numbers = django_filters.CharFilter(field_name='numbers__number', lookup_expr='icontains')
+    search = django_filters.CharFilter(method='filter_by_all')
 
     class Meta:
         model = models.Client
         fields = ['region', 'orders__status', 'full_name', 'code_number', 'phone_numbers']
+
+    def filter_by_all(self, queryset, name, value):
+        return queryset.filter(
+            Q(full_name__icontains=value) |
+            Q(code_number__icontains=value) |
+            Q(numbers__number__icontains=value)
+        ).distinct()
