571afdb5b9de3b21c41168fb2c272110fdca75af
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-04-24 16:52:21 +0500

    fix

--- common/filters.py
+++ common/filters.py
@@ -6,10 +6,11 @@ from common import models
 
 class ClientFilter(django_filters.FilterSet):
     search = django_filters.CharFilter(method='filter_by_all')
+    is_new = django_filters.BooleanFilter(method='filter_by_new')
 
     class Meta:
         model = models.Client
-        fields = ['region', 'orders__status', 'search']
+        fields = ['region', 'is_new', 'search']
 
     def filter_by_all(self, queryset, name, value):
         return queryset.filter(
@@ -17,3 +18,8 @@ class ClientFilter(django_filters.FilterSet):
             Q(code_number__icontains=value) |
             Q(numbers__number__icontains=value)
         ).distinct()
+    
+    def filter_by_new(self, querysey, name, value):
+        return querysey.filter(
+            orders__status='yangi'
+        )
