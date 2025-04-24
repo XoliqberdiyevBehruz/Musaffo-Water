65a00ae8c7ebca76216c782c145f9d3ede285cb5
behruz-dev <xoliqbediyevbehruz@gmail.com>
2025-04-24 16:11:32 +0500

    fix

--- common/serializers.py
+++ common/serializers.py
@@ -217,7 +217,7 @@ class ClientOrderUpdateSerializer(serializers.ModelSerializer):
         instance.paid = validated_data.get('paid', instance.paid)
         instance.status = validated_data.get('status', instance.status)
 
-        instance.the_rest = instance.count - instance.received + validated_data['previous_order'].the_rest
+        instance.the_rest = instance.count - instance.received + self.context['previous_order'].the_rest
 
         # Saqlash
         instance.save()
